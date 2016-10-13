e710d6f68d58ac581a0ba54775f23e0696109014
Alexander Günsche <mail@lxg.de>
2016-10-13 21:00:24 +0200

    add twig extension for base URL

---
+++ lib/Service/TwigExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * @package    agitation/base-bundle
+ * @link       http://github.com/agitation/base-bundle
+ * @author     Alexander Günsche
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\BaseBundle\Service;
+
+use Agit\BaseBundle\Service\UrlService;
+use Twig_Extension;
+use Twig_SimpleFunction;
+
+class TwigExtension extends Twig_Extension
+{
+    private $urlService;
+
+    public function __construct(UrlService $urlService)
+    {
+        $this->urlService = $urlService;
+    }
+
+    public function getName()
+    {
+        return "agit.base";
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new Twig_SimpleFunction("getAppUrlBase", [$this, "getAppUrlBase"])
+        ];
+    }
+
+    public function getAppUrlBase()
+    {
+        return $this->urlService->createAppUrl("/");
+    }
+}
