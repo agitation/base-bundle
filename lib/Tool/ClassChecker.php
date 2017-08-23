900091c24c9adb0442dc9b04ef47e191ed84604a
Alexander Günsche <mail@lxg.de>
2017-08-23 08:37:29 +0200

    add tool for validating object arrays

---
+++ lib/Tool/ClassChecker.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * @package    agitation/base-bundle
+ * @link       http://github.com/agitation/base-bundle
+ * @author     Alexander Günsche
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\BaseBundle\Tool;
+
+use RuntimeException;
+
+class ClassChecker
+{
+    /**
+     * Checks that a passed argument is an array and every array member is an
+     * instance of $className. Basically the same as type-hinting for Class[].
+     *
+     * @param array|ArrayObject $array
+     * @param string            $className
+     *
+     * @throws RuntimeException
+     */
+    public static function isObjectArray($array, $className)
+    {
+        if (! is_array($array)) {
+            throw new RuntimeException("The variable must be an array.");
+        }
+
+        foreach ($array as $value) {
+            if (! ($value instanceof $className)) {
+                throw new RuntimeException(sprintf("The variable must be an instance of %s.", $className));
+            }
+        }
+    }
+}
