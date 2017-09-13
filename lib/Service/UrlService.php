0b92c41c8b88386d278b0923fbdb01796bd03fc9
Alexander Günsche <mail@lxg.de>
2017-09-13 09:25:47 +0200

    remove domain name awareness; will be enforced/filtered by webserver

--- lib/Service/UrlService.php
+++ lib/Service/UrlService.php
@@ -9,8 +9,6 @@ declare(strict_types=1);
 
 namespace Agit\BaseBundle\Service;
 
-use Agit\BaseBundle\Exception\InternalErrorException;
-
 class UrlService
 {
     public function createAppUrl($path = '', array $params = [])
